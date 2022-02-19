1b1b462bea2e6d328d5ca8c6297326207dd00050
kimhyoeun <khyoeun92@gmail.com>
2022-02-19 21:55:33 +0900

    단어순서 뒤집기 - 파이썬

--- 7weeks/단어순서 뒤집기.py
+++ 7weeks/단어순서 뒤집기.py
@@ -9,3 +9,17 @@
 # 1 ≤ L ≤ 25
 # 출력
 # 각 케이스에 대해서, 케이스 번호가 x일때  "Case #x: " 를 출력한 후 그 후에 이어서 단어들을 반대 순서로 출력한다.
+
+
+N = int(input()) # 전체 케이스 개수
+word_dict = {} # 입력 받은 단어를 넣을 딕셔너리. key는 케이스, value는 입력 받은 단어.
+
+for i in range(N):
+  word_dict[i+1] = input()
+
+for key, value in word_dict.items(): # 위의 word_dict를 반복하면서 key와 value를 모두 활용
+  split_word_list = value.split( ) # 단어를 띄어쓰기 단위로 쪼개서 배열에 넣어줌
+  temp_list = []
+  for index in reversed(range(len(split_word_list))): # 띄어쓰기 단위로 단어를 쪼갠 리스트의 index를 활용. 역순으로.
+    temp_list.append(split_word_list[index]) # 그걸 임시 배열에 담기.
+  print('Case #%d:' % key, ' '.join(temp_list)) # 문제 형식에서 원하는대로 출력
