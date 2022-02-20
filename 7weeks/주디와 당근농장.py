20d28d5391ae2e79f1c1c9c34cd500dd3a911912
kimhyoeun <khyoeun92@gmail.com>
2022-02-20 09:44:44 +0900

    주디와 당근농장 - 파이썬

--- 7weeks/주디와 당근농장.py
+++ 7weeks/주디와 당근농장.py
@@ -10,5 +10,32 @@
 # 첫 번째 줄부터 N번째 줄까지 각 줄에 길이가 N인 문자열을 출력합니다.
 # i행 j열에 당근이 심어졌다면 i번째 줄 j번째 문자를 'v' 로 표시하며 심어지지 않았다면 '.' 로 표시합니다.
 
+N, R, C = list(map(int, input().split())) # 가로세로 값, 세로 좌표 값, 가로 좌표 값
 
+carrot_farm = [['.' for _ in range(N)] for _ in range(N)] # N x N 만큼의 당근 농장을 만들기. 기본 값은 .로 세팅
+
+# 세로가 (배열 인덱스 기준으로) 홀수고, 가로가 (배열 인덱스 기준으로) 짝수일 때
+# 세로가 (배열 인덱스 기준으로) 홀수고, 가로가 (배열 인덱스 기준으로) 홀수일 때
+if ((R-1) % 2 != 0 and (C-1) % 2 == 0) or ((R-1) % 2 == 0 and (C-1) % 2 != 0):
+  for i in range(N):
+    for j in range(N):
+      if i % 2 == 0 and j % 2 != 0:
+        carrot_farm[i][j] = 'v'
+      elif i % 2 != 0 and j % 2 == 0:
+        carrot_farm[i][j] = 'v'
+
+# 세로가 (배열 인덱스 기준으로) 홀수고, 가로가 (배열 인덱스 기준으로) 홀수일 때
+# 세로가 (배열 인덱스 기준으로) 짝수고, 가로가 (배열 인덱스 기준으로) 짝수일 때
+elif ((R-1) % 2 != 0 and (C-1) % 2 != 0) or ((R-1) % 2 == 0 and (C-1) % 2 == 0):
+  for i in range(N):
+    for j in range(N):
+      if i % 2 == 0 and j % 2 == 0:
+        carrot_farm[i][j] = 'v'
+      elif i % 2 != 0 and j % 2 != 0:
+        carrot_farm[i][j] = 'v'
+
+convert1, convert2 = "", " \n"
+carrot_farm = convert2.join([convert1.join([str(elem) for elem in sub]) for sub in carrot_farm])
+
+print(carrot_farm)
 
